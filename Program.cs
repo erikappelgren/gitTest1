96ae2de2ff9d3f65f11f62dced8c8ec5abcdf7fd
erikappelgren123@gmail.com <erikappelgren123@gmail.com>
2020-10-22 10:49:39 +0200

    loop

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace gitTest1
             //Console.WriteLine("Hello world");
             for (int i = 1; i <= 10; i++)
             {
-                Console.WriteLine("{0}^2 = {1}", i, i * i);
+                Console.WriteLine("{0}^2 = {1}, {0}^3 = {2}", i, i * i, i * i * i);
             }
         }
     }
